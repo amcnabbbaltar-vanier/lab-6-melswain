6c1204c52b01c1e01b62d0d784d7b9c7848d7f5d
Melanie Swain <6228040@edu.vaniercollege.qc.ca>
2025-10-06 12:44:07 -0400

    Completed up until pause manager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public Text scoreText;
-    private int score = 0;
+    public int score = 0;
     public int targetScore = 4;
 
 
